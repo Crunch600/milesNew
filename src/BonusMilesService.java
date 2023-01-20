ba78c915dd304d565afbe3d5d4a719b1237e4892
Dmitrii Yablochkin <yablochkind89@gmail.com>
2023-01-20 21:05:00 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int miles;
+        int bonus = 20;
+
+        miles = cost / bonus;
+
+        return miles;
+    }
+}
